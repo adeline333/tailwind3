e7bda2630ee7ee7bf87302c686ce6c0a4110fce3
AdelineTuyizere <adelinetuyizere333@gmail.com>
2025-01-16 17:26:57 +0200

    started working on the landing page exercise

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: [
+    './*.{html,js}',  // Looks for all .html and .js files in the root folder
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
